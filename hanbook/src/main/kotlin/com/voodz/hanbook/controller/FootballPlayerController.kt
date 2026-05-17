ba2d5c492c53190eb7f3b66d04e24662a4d1c1f4
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2026-05-17 13:11:29 +0300

    add controller

---
+++ hanbook/src/main/kotlin/com/voodz/hanbook/controller/FootballPlayerController.kt
@@ -0,0 +1,52 @@
+package com.voodz.hanbook.controller
+
+import com.voodz.hanbook.model.FootballPlayer
+import com.voodz.hanbook.repository.FootballClubRepository
+import com.voodz.hanbook.repository.FootballPlayerRepository
+import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Controller
+@RequestMapping("/players")
+class FootballPlayerController(
+    private val playerRepository: FootballPlayerRepository,
+    private val clubRepository: FootballClubRepository,
+) {
+    @GetMapping
+    fun listPlayers(
+        @RequestParam(defaultValue = "lastName") sortBy: String,
+        model: Model,
+    ): String {
+       val sort = Sort.by(sortBy)
+       model.addAttribute("players", playerRepository.findAll(sort))
+        model.addAttribute("studentInfo", "Войтукевич Р.Ю., 3 курс, 2 группа, 2026")
+        return "players/list"
+    }
+
+    @GetMapping("/add")
+    fun showAddForm(model: Model): String {
+        model.addAttribute("player", FootballPlayer()) // Пустой объект для формы
+        model.addAttribute("clubs", clubRepository.findAll()) // Список для выпадающего меню
+        return "players/form"
+    }
+
+    @PostMapping("/save")
+    fun savePlayer(@ModelAttribute("player") player: FootballPlayer): String {
+        playerRepository.save(player)
+        return "redirect:/players"
+    }
+
+    // 4. Удаление
+    @GetMapping("/delete/{id}")
+    fun deletePlayer(@PathVariable id: Long): String {
+        playerRepository.deleteById(id)
+        return "redirect:/players"
+    }
+}
